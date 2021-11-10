ce0f6419c9879e38954dc927c8455d343bedd99c
Stephen Moore <judo2000@comcast.net>
2021-11-10 14:57:39 -0700

    Update startGame() function and added a play again button to renderScores() function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 // target carosell div
 const startBtn = document.getElementById("startBtn");
+const playAgainBtn = document.getElementById("playAgainBtn");
+playAgainBtn.style.display = "none";
 let carousel = document.querySelector("#carouselbox");
 // create h2 element
 let h2El = document.createElement("h2");
@@ -7,12 +9,16 @@ let h2El = document.createElement("h2");
 let listEl = document.createElement("ul");
 // set index to 0 so the first question will display in the crousel
 let index = 0;
+var secondsLeft ;
 // initialize currentQuestion variable
 let currentQuestion;
 // initialize score and set to 0
 let score = 0;
+let highScores = [];
+let gameOver = true;
 var timeEl = document.querySelector(".time");
 let results = document.querySelector("#results");
+
 //let secondsLeft = 10;
 
 
@@ -71,27 +77,27 @@ function next() {
   } 
 }
 
-function saveScore() {
+// function saveScore() {
   
-  let initials = document.getElementById('userInitials');
+//   let initials = document.getElementById('userInitials').value;
+//   console.log(initials);
   
-  
-  console.log(initials);
-  var user = {
-    initials: initials.value,
-    score: 2
-  };
-  // Get the existing data
-  var storedUser = localStorage.getItem('user');
-  console.log(storedUser);
-localStorage.setItem("user", JSON.stringify(user));
-  renderScores();
-}
+//   var user = {
+//     initials: initials,
+//     score: 2
+//   };
+//   // Get the existing data
+//   var storedUser = localStorage.getItem('user');
+//   console.log(storedUser);
+//   localStorage.setItem("user", JSON.stringify(user));
+//   //renderScores();
+// }
+
 var saveBtn = document.createElement("button");
+
 function showResults() {
   listEl.innerHTML = "";
   h2El.textContent = `You scored ${score}!`;
-  
   // add form for initials here
   
   var userInitials = document.createElement("input");
@@ -101,11 +107,31 @@ function showResults() {
   
   saveBtn.textContent = "Save";
   results.appendChild(saveBtn);
-  
+  console.log("showing results");
 }
 
 saveBtn.addEventListener('click', function() {
+  let initials = document.getElementById('userInitials').value;
+  console.log(initials);
+  
+  var userScore = {
+    initials: initials,
+    score: score
+  };
+
+  var storedScores = JSON.parse(localStorage.getItem("scores"));
+
+  // If todos were retrieved from localStorage, update the todos array to it
+  if (storedScores !== null) {
+    highScores = storedScores;
+  } 
+  highScores.push(userScore);
+
+  console.log(highScores);
+  // Get the existing data
+  localStorage.setItem("scores", JSON.stringify(highScores));
   results.innerHTML = "";
+  renderScores();
 });
 
 carousel.addEventListener('click', function(event) {
@@ -115,30 +141,48 @@ carousel.addEventListener('click', function(event) {
   if (element.matches("button") === true) {
     // console.log("it is a button");
     let elIndex = element.getAttribute("data-index");
-    console.log(elIndex);
+    //console.log(elIndex);
     let answer = questions[index].answer;
     if (parseInt(elIndex) === answer) {
       score++;
-    } else {
-      secondsLeft -= 10;
-      setTime();
-    }
-    console.log("index ", index);
-    console.log("User Chose", elIndex);
-    console.log("answer", answer);
-    console.log("THE INDEX IS ", index);
-    index = index + 1;
+      index = index + 1;
     if (index !== questions.length) {
       next();
     } else {
+      secondsLeft = 0;
+      //clearInterval(timerInterval);
       timeEl.style.display = 'none';
-      showResults();
+      
+      if (gameOver === false) {
+        gameOver = true;
+        showResults();
+      }
     }
+    } else {
+      secondsLeft -= 10;
+      //setTime();
+      index = index + 1;
+    if (index !== questions.length) {
+      next();
+    } else {
+      secondsLeft = 0;
+      //clearInterval(timerInterval);
+      timeEl.style.display = 'none';
+      
+      if (gameOver === false) {
+        gameOver = true;
+        showResults();
+      }
+    }
+    }
+    // console.log("index ", index);
+    // console.log("User Chose", elIndex);
+    // console.log("answer", answer);
+    // console.log("THE INDEX IS ", index);
+    
   }
   
 });
-
-var secondsLeft = 60;
 
 function setTime() {
   // Sets interval in variable
@@ -146,23 +190,55 @@ function setTime() {
     secondsLeft--;
     timeEl.textContent = secondsLeft + " seconds";
     
-    if(secondsLeft === 0) {
+    if(secondsLeft <= 0) {
       // Stops execution of action at set interval
+      clearInterval(timerInterval);
       timeEl.style.display = 'none';
       // Calls function to create and append image
-      showResults();
+      if (gameOver === false) {
+        gameOver = true;
+        showResults();
+      }
     }
 
   }, 1000);
 }
 
+
+
 startBtn.addEventListener('click', function() {
-  timeEl.textContent = secondsLeft + " seconds";
-  setTime();
-  startBtn.style.display = 'none';
-  buildList();
+  startGame();
 });
 
+
 function renderScores() {
-  var lastGrade = JSON.parse(localStorage.getItem("studentGrade"));
+   h2El.textContent = `High Scores!`;
+   for (var i = 0; i < highScores.length; i++) {
+    listItem = document.createElement("li");
+    listItem.textContent = `${highScores[i].initials} - ${highScores[i].score}`;
+    listEl.appendChild(listItem);
+    results.appendChild(listEl);
+  }
+  results.appendChild(playAgainBtn);
+  playAgainBtn.style.display = "block";
 }
+
+playAgainBtn.addEventListener('click', function() {
+  playAgainBtn.style.display = "none";
+  startGame();
+});
+
+function startGame() {
+  highScores = [];
+  index = 0;
+  score = 0;
+  timeEl.style.display = 'block';
+  secondsLeft = 60;
+  setTime();
+  gameOver = false;
+  timeEl.textContent = secondsLeft + " seconds";
+  
+  startBtn.style.display = 'none';
+  buildList();
+}
+
